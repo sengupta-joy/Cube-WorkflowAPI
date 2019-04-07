90bc6c895c28d6b8d72382be5522eadd8a02abff
joy.sengupta <joy.sengupta@live.com>
2019-04-07 23:22:26 +0300

    user

--- CUBE-WorkflowAPI/CUBE-WorkflowAPI/App_Code/UserSession.vb
+++ CUBE-WorkflowAPI/CUBE-WorkflowAPI/App_Code/UserSession.vb
@@ -7,11 +7,102 @@ Imports App_Code.DB
 <Serializable>
 Public Class UserInfo
 
+    Private _nm As String
+    Private _id As String
+    Private _eml As String
+    Private _actv As Boolean
+    Private _desig As String
+    Private _dept As String
+    Private _brn As String
+    Private _bs As String
+    Private _comp As String
+
+    Public Sub New(uid As String)
+
+    End Sub
 
 
-    Public Name As String = "joy"
-    Public id As Integer = 1234
+    Public Property Name As String
+        Get
+            Return _nm
+        End Get
+        Set(value As String)
+            _nm = value
+        End Set
+    End Property
 
+    Public ReadOnly Property Id As String
+        Get
+            Return _id
+        End Get
+    End Property
+
+    Public Property Email As String
+        Get
+            Return _eml
+        End Get
+        Set(value As String)
+            _eml = value
+        End Set
+    End Property
+
+    Public Property Active As Boolean
+        Get
+            Return _actv
+        End Get
+        Set(value As Boolean)
+            _actv = value
+        End Set
+    End Property
+
+    Public Property Designation As String
+        Get
+            Return _desig
+        End Get
+        Set(value As String)
+            _desig = value
+        End Set
+    End Property
+
+    Public Property Depertment As String
+        Get
+            Return _dept
+        End Get
+        Set(value As String)
+            _dept = value
+        End Set
+    End Property
+
+    Public Property Branch As String
+        Get
+            Return _brn
+        End Get
+        Set(value As String)
+            _brn = value
+        End Set
+    End Property
+
+    Public Property Boss As String
+        Get
+            Return _bs
+        End Get
+        Set(value As String)
+            _bs = value
+        End Set
+    End Property
+
+    Public Property Company As String
+        Get
+            Return _comp
+        End Get
+        Set(value As String)
+            _comp = value
+        End Set
+    End Property
+
+    Public Function saveUser() As Boolean
+
+    End Function
 
     Public Shared Function validateLogin(uid As String, password As String) As UserInfo
         Dim dl As New DBContext(USERDB)
@@ -30,14 +121,11 @@ Public Class UserInfo
         param.SqlDbType = SqlDbType.NVarChar
         params.Add(param)
 
-        param = New SqlClient.SqlParameter()
-        param.ParameterName = "@TOKEN"
-        param.Value = ""
-        param.SqlDbType = SqlDbType.NVarChar
-        param.Direction = ParameterDirection.Output
-        params.Add(param)
 
-        dl.ExecuteSP("[SP_VALIDATE_LOGIN]", CommandType.StoredProcedure, params)
+
+        Dim Response = dl.ExecuteSP("[SP_VALIDATE_LOGIN]", CommandType.StoredProcedure, params)
+
+        Return New UserInfo(Response.Data.Tables(0).Rows(0)(0))
 
         Return Nothing
     End Function
